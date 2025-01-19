b0893cb08c3ef552e7e77bd958f23ed4365bdc38
Curran <curran.kelleher@gmail.com>
2025-01-19 16:09:38 -0500

    Update config for domain launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://curran.github.io",
-  base: "/currankelleher.com/",
+  site: "https://currankelleher.com",
 });
